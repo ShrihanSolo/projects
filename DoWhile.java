b455bd89f541cb5a27ef37d13e30b9dce198dee0
ShrihanSolo <shrihan@berkeley.edu>
2023-12-10 23:18:14 +0530

    adding java

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+public class DoWhile {
+  public static void main(String[] args) {
+    int i = 0;
+    do {
+      i += 1;
+      System.out.println(i);
+    } while (i < 10);
+    i = 0;
+    while (i < 10) {
+      i += 1;
+      System.out.println(i);
+    }
+  }
+}
